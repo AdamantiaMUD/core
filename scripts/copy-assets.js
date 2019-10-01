a16632b9e0e7ccac4e25566b6c84bcc65455cdbe
Bill Parrott <bill@chimericdream.com>
2019-10-01 00:10:18 -0500

    copying assets

---
+++ scripts/copy-assets.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const motdDestFolder = path.join(__dirname, '..', 'build', 'core-bundles', 'input-events', 'resources');
+const motdDest = path.join(motdDestFolder, 'motd');
+const motdSrc = path.join(__dirname, '..', 'src', 'core-bundles', 'input-events', 'resources', 'motd');
+
+fs.mkdirSync(motdDestFolder, {recursive: true});
+fs.copyFileSync(motdSrc, motdDest);
