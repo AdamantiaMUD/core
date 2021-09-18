62314a13a66a7b8bd3c487749759e9c51883bdff
Bill Parrott <bill@mythical.games>
2021-09-17 23:31:46 -0500

    chore: dependency updates

--- types/vendor.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'rando-js' {
-    function coinFlip(): 0 | 1;
-    function fromArray<T = unknown>(arr: T[]): T;
-    function inRange(min: number, max: number): number;
-    function probability(chance: number): boolean;
-    function roll(count: number, sides: number): number;
-}
